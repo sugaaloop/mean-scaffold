031f1518cdc496bfc637d91c3dca45c2450bfab2
sugaaloop <hennessey.re@gmail.com>
2016-12-18 14:31:32 -0500

    moving back to external build server

--- build.sh
+++ build.sh
@@ -14,17 +14,15 @@ tar -cf build.tar ./app ./config ./public ./server.js
 echo "archived the app"
 
 # backup and clean dest
-#ssh -i /var/lib/jenkins/.ssh/id_rsa webadmin@bobhennessey.net '/var/www/scripts/prepare-build.sh'
+ssh -i /var/lib/jenkins/.ssh/id_rsa webadmin@bobhennessey.net '/var/www/scripts/prepare-build.sh'
+echo "ran prepare-build.sh"
 
 # copy tarball to dest
-#scp -i /var/lib/jenkins/.ssh/id_rsa ./build.tar webadmin@bobhennessey.net:/var/www/bobhennessey.net-STAGING
-mkdir -p /var/www/bobhennessey.net-STAGING
-cp ./build.tar /var/www/bobhennessey.net-STAGING/build.tar
+scp -i /var/lib/jenkins/.ssh/id_rsa ./build.tar webadmin@bobhennessey.net:/var/www/bobhennessey.net-STAGING/build.tar
 echo "copied the app"
 
-# extract the app
-cd /var/www/bobhennessey.net-STAGING
-tar -xf ./build.tar
+# extract at target
+ssh -i /var/lib/jenkins/.ssh/id_rsa webadmin@bobhennessey.net '/var/www/scripts/extract-build.sh'
 echo "unarchived the app"
 
 # restart web server
