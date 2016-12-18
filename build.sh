e701f4c4a8a520a038a34a42b739c65184c6aec8
sugaaloop <hennessey.re@gmail.com>
2016-12-17 21:16:17 -0500

    build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ npm install
 gulp --prod
 
 # tarball the app
-tar -cf build.tar ./app ./config ./node_modules ./public ./server.js
+tar -cf build.tar ./app ./config ./public ./server.js
 
 # backup and clean dest
 #ssh -i /var/lib/jenkins/.ssh/id_rsa webadmin@bobhennessey.net '/var/www/scripts/prepare-build.sh'
