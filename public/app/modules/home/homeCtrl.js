47131a62d182f851f2e76896ebc2f4726a616324
sugaaloop <hennessey.re@gmail.com>
2017-01-30 18:38:44 -0500

    testing coCORS to pantheon headless drupal

--- public/app/modules/home/homeCtrl.js
+++ public/app/modules/home/homeCtrl.js
@@ -1,9 +1,12 @@
 (function () {
     'use strict';
     angular.module('net.bobhennessey.home').controller('homeCtrl', homeCtrl);
-    homeCtrl.$inject = [];
-    function homeCtrl() {
+    homeCtrl.$inject = ['$http'];
+    function homeCtrl($http) {
         var vm = this;
         vm.title = "bobhennessey.net";
+        vm.blogs;
+
+        $http.get('http://dev-bhnet-headless.pantheonsite.io/blogs').then(function (response) { vm.blogs = response.data; });
     }
 })();
