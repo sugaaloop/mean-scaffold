b66be79987c581b5f7052ea700452b7a082074f7
sugaaloop <hennessey.re@gmail.com>
2017-02-18 21:13:57 -0500

    content component

--- public/app/modules/home/blogs/component.js
+++ public/app/modules/home/blogs/component.js
@@ -11,26 +11,7 @@
         vm.something = 'adsf';
 
         vm.$onInit = function() {
-            drupal.connect().then(function() {
-                var user = drupal.currentUser();
-                var authenticated = user.isAuthenticated();
-                console.log(user, authenticated);
-
-                drupal.loadNode(4).then(function (node){
-                    console.log(node);
-                });
-            });
-
-            drupal.viewsLoad('api/blogs').then(function(response) {
-                $scope.$apply(function() {
-                    vm.blogs = response.results;
-                });
-            });
-
-            // $http.get('http://dev-bhnet-headless.pantheonsite.io/api/blogs').then(function (response) {
-            //     vm.blogs = response.data;
-            //     console.log(response.data);
-            // });
+            
         }
     }
 })();
